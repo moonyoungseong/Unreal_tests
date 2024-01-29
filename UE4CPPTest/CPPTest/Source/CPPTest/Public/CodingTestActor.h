34656902ca50dff905b0236211c1c467473de311
moonyoungseong <ansdudtjd123@naver.com>
2024-01-29 13:06:20 +0900

    변수 연습
    출력 연습

---
+++ UE4CPPTest/CPPTest/Source/CPPTest/Public/CodingTestActor.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CodingTestActor.generated.h"
+
+UCLASS()
+class CPPTEST_API ACodingTestActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACodingTestActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// 정수형 int32로 int도 가능한데 나중에 사용하는 기능에 제약 생김
+	int32 number1 = 85;
+};
