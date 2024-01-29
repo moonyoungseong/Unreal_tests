7c32a4ca697db27b24bdf05dff7a961e08ea56bf
moonyoungseong <ansdudtjd123@naver.com>
2024-01-29 12:23:27 +0900

    프로젝트 생성
    생성 및 설정

---
+++ UE4CPPTest/CPPTest/Source/CPPTest/CPPTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPPTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPTEST_API ACPPTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
