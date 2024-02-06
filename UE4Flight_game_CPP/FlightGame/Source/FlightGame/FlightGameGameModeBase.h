987d3326732d42a16979ca9301dfd119cecc23ec
moonyoungseong <ansdudtjd123@naver.com>
2024-02-06 18:43:38 +0900

    프로젝트 생성
    생성 및 설정

---
+++ UE4Flight_game_CPP/FlightGame/Source/FlightGame/FlightGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlightGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLIGHTGAME_API AFlightGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
