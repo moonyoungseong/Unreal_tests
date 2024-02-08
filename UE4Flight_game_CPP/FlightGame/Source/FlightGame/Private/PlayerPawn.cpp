055a6da3e68150d57051388f91724f3ee2ccbfa9
moonyoungseong <ansdudtjd123@naver.com>
2024-02-08 20:09:19 +0900

    콜라이더, 메시
    플레이어 생성

---
+++ UE4Flight_game_CPP/FlightGame/Source/FlightGame/Private/PlayerPawn.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPawn.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 박스 콜라이더 컴포넌트를 생성한다.
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("My Box Component"));
+
+	// 생성한 박스 콜라이더 컴포넌트를 최상단 컴포넌트로 설정
+	SetRootComponent(boxComp);
+
+	// 스태틱 메시 컴포넌트를 생성한다.
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My Static Mesh"));
+
+	// 박스 콜라이더 컴포넌트의 자식 컴포넌트로 설정
+	meshComp->SetupAttachment(boxComp);
+
+	// 박스 콜라이더의 크기를 50으로 수정
+	FVector boxSize = FVector(50.0f, 50.0f, 50.0f);
+	boxComp->SetBoxExtent(boxSize);
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ UE4Flight_game_CPP/FlightGame/Source/FlightGame/Public/PlayerPawn.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class FLIGHTGAME_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// 박스 충돌체 컴포넌트
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* boxComp;
+
+	// 스태틱 메시 컴포넌트
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* meshComp;
+};
