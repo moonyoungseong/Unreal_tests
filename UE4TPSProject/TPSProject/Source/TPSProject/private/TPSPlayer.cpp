f17bf6a4e372d5f1fc360dee1d5251a0bb492904
moonyoungseong <ansdudtjd123@naver.com>
2024-02-13 12:40:41 +0900

    외관, 카메라
    카메라 추가 작업 필요

--- UE4TPSProject/TPSProject/Source/TPSProject/private/TPSPlayer.cpp
+++ UE4TPSProject/TPSProject/Source/TPSProject/private/TPSPlayer.cpp
@@ -2,6 +2,8 @@
 
 
 #include "TPSPlayer.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
 
 // Sets default values
 ATPSPlayer::ATPSPlayer()
@@ -9,6 +11,22 @@ ATPSPlayer::ATPSPlayer()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 1.스켈레탈 메시 데이터를 불러오고 싶다.
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempMesh(TEXT("SkeletalMesh'/Game/Mannequin/Character/Mesh/SK_Mannequin.SK_Mannequin'"));
+	if (TempMesh.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(TempMesh.Object);
+		// Mesh 컴포넌트 위치 설정
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -90), FRotator(0, -90, 0));
+	}
+
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	springArmComp->SetupAttachment(RootComponent);
+	springArmComp->SetRelativeLocation(FVector(0, 70, 90));
+	springArmComp->TargetArmLength = 400;
+
+	tpsCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("TpsCamComp"));
+	tpsCamComp->SetupAttachment(springArmComp);
 }
 
 // Called when the game starts or when spawned
